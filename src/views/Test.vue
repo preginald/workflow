c8b7b6b9f4b32cdd41c9934bc1cf24283998d254
Peter Reginald <peter@digitalsanctum.com>
2020-12-13 19:06:59 +1100

    add Stepper component

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -6,33 +6,7 @@
       </v-col>
     </v-row>
     <Form />
-    <v-stepper v-model="step">
-      <v-stepper-header>
-        <template v-for="step in doc.steps">
-          <v-stepper-step :key="step.order" :step="step.order" editable>
-            {{ step.title }}
-          </v-stepper-step>
-          <v-divider
-            v-if="step.order !== doc.steps.length"
-            :key="step.title"
-          ></v-divider>
-        </template>
-      </v-stepper-header>
-      <v-stepper-items v-for="step in doc.steps" :key="step.order">
-        <v-stepper-content :step="step.order">
-          <v-row>
-            <v-col>
-              <h3>{{ step.title }}</h3>
-            </v-col>
-          </v-row>
-          <v-row v-for="task in step.tasks" :key="task.order">
-            <v-col>
-              <kbd> {{ taskInterpreter(task.title) }}</kbd>
-            </v-col>
-          </v-row>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
+    <Stepper />
   </v-container>
 </template>
 
@@ -40,56 +14,14 @@
 // @ is an alias to /src
 import { mapState } from "vuex";
 import Form from "@/components/Form";
+import Stepper from "@/components/Stepper";
 
 export default {
   computed: mapState(["doc"]),
   name: "Home",
-  components: { Form },
+  components: { Form, Stepper },
   data: () => ({
     step: 1,
   }),
-  methods: {
-    taskInterpreter(task) {
-      let n = task.search("<variable>");
-      if (n == -1) {
-        return task;
-      } else {
-        let n = task.split("</variable>");
-        let newTask = "";
-        n.forEach((e) => {
-          let x = e.search("<variable>");
-          if (x == -1) {
-            newTask += e;
-          } else {
-            let variable = this.getVariable(e);
-            let value = this.getValue(variable);
-            newTask += e.replace("<variable>" + variable, value);
-          }
-        });
-        return newTask;
-      }
-    },
-    getVariable(task) {
-      let n = task.search("<variable>");
-      if (n > -1) {
-        let variable = task.substring(n + 10, task.length);
-        return variable;
-      }
-    },
-    getValue(name) {
-      let n = this.doc.inputs.find((e) => e.name == name);
-      return n.value;
-    },
-    taskInterpreterBackup(task) {
-      let n = task.search("<variable>");
-      if (n == -1) {
-        return task;
-      } else {
-        let variable = this.getVariable(task);
-        let value = this.getValue(variable);
-        return task.replace("<variable>" + n + "</variable>", value);
-      }
-    },
-  },
 };
 </script>
