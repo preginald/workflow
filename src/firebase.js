a96db17218fe0d8cfaf3157c0ddd93e1e41e9dc0
preginald <peter@reginald.com.au>
2021-01-02 23:32:10 +0000

    move firebase settings to config file

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// firebase init - add your own config here
+const firebaseConfig = {
+  apiKey: "AIzaSyCRikWNRskGXxWcvmk-Lj9mDV96TOLrSHg",
+  authDomain: "workflow-a.firebaseapp.com",
+  projectId: "workflow-a",
+   storageBucket: "workflow-a.appspot.com",
+   messagingSenderId: "612098345904",
+   appId: "1:612098345904:web:a7df21a1193600d1eee475",
+   measurementId: "G-HVBJCNKTDC"
+ };
+
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// collection references
+const usersCollection = db.collection('users')
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+}
+
--- src/main.js
+++ src/main.js
@@ -4,26 +4,21 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from "axios"
-import * as firebase from 'firebase'
+import { auth } from './firebase'
+
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCRikWNRskGXxWcvmk-Lj9mDV96TOLrSHg",
-  authDomain: "workflow-a.firebaseapp.com",
-  projectId: "workflow-a",
-   storageBucket: "workflow-a.appspot.com",
-   messagingSenderId: "612098345904",
-   appId: "1:612098345904:web:a7df21a1193600d1eee475",
-   measurementId: "G-HVBJCNKTDC"
- };
-// Initialize Firebase
-firebase.default.initializeApp(firebaseConfig);
-
-new Vue({
+let app
+auth.onAuthStateChanged(() => {
+  if (!app) {
+app = new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+}
+})
+
