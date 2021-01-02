e006dd3a8b8848b8ca01084125ba70aa869bcba9
preginald <peter@reginald.com.au>
2021-01-02 09:13:57 +0000

    add registration with firebase auth

--- src/main.js
+++ src/main.js
@@ -4,24 +4,22 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from "axios"
-import firebase from 'firebase/app'
+import * as firebase from 'firebase'
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCRikWNRskGXxWcvmk-Lj9mDV96TOLrSHg",
-    authDomain: "workflow-a.firebaseapp.com",
-    projectId: "workflow-a",
-    storageBucket: "workflow-a.appspot.com",
-    messagingSenderId: "612098345904",
-    appId: "1:612098345904:web:a7df21a1193600d1eee475",
-    measurementId: "G-HVBJCNKTDC"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCRikWNRskGXxWcvmk-Lj9mDV96TOLrSHg",
+  authDomain: "workflow-a.firebaseapp.com",
+  projectId: "workflow-a",
+   storageBucket: "workflow-a.appspot.com",
+   messagingSenderId: "612098345904",
+   appId: "1:612098345904:web:a7df21a1193600d1eee475",
+   measurementId: "G-HVBJCNKTDC"
+ };
+// Initialize Firebase
+firebase.default.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,18 +5,32 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-form>
+      <v-form @submit.prevent="userRegistration">
         <v-container>
           <v-row>
-            <v-col cols="12" md="6">
+            <v-col v-if="error">
+              {{ error.message }}
+            </v-col>
+
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="4">
               <v-text-field
+                type="email"
+                v-model="user.email"
+                required
                 label="email"
               ></v-text-field>
             </v-col>
-            <v-col cols="12" md="6">
+            <v-col cols="12" md="4">
               <v-text-field
+                type="password"
+                v-model="user.password"
                 label="password"
               ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-btn type="submit">Register</v-btn>
             </v-col>
           </v-row>
         </v-container>
@@ -30,14 +44,37 @@
 </template>
 
 <script>
-// @ is an alias to /src
+//import * as firebase from "firebase/app"
+import firebase from "firebase"
 
 export default {
   name: "Register",
   components: {},
   data: () => ({
+    user: {
+      email: "",
+      password: "",
+      error: "",
+      },
   }),
   methods: {
-    },
+    userRegistration() {
+      firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.user.email, this.user.password)
+      .then((res) => {
+        res.user
+          .updateProfile({
+            displayName: this.user.name
+          })
+          .then(() => {
+            this.$router.push('/login')
+          });
+      })
+      .catch((error) => {
+         alert(error.message);
+      });
+    }
+  },
 };
 </script>
