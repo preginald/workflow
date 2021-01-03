430a65803ab0ea94950378b3ce92f3a8d21501e0
preginald <peter@reginald.com.au>
2021-01-03 11:21:00 +0000

    add new firestore actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import * as fb from '../firebase'
+import router from '../router/index'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     user: null,
+    userProfile: {},
     doc: {
       username: 'preginald',
       title: 'Install Wordpress on Apache2 VirtualHost',
@@ -181,7 +184,48 @@ export default new Vuex.Store({
     setUser(state, user){
       state.user = user
     },
+    setUserProfile(state, user){
+      state.userProfile = user
+    },
   },
-  actions: {},
+  actions: {
+    async login({ dispatch }, form) {
+      // sign user in
+      const { user } = await fb.auth.signInWithEmailAndPassword(form.email, form.password)
+      
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    },
+    async logout({ commit }){
+      alert('lele')
+      await fb.auth.signOut()
+
+      // clear userProfile and redirect to /login
+      commit('setUserProfile', {})
+      router.push('/login')
+    },
+    async fetchUserProfile({ commit }, user){
+      // fetch user profile
+      const userProfile = await fb.usersCollection.doc(user.uid).get()
+
+      // set user profile in state
+      commit('setUserProfile', userProfile.data())
+
+      // change route to dashboard
+       router.push({ name: 'user', params: { userName: userProfile.data().username}})
+    },
+    async signup({ dispatch }, form){
+      // sign user up
+      const { user } = await fb.auth.createUserWithEmailAndPassword(form.email, form.password)
+
+      // create user profile object in userCollection
+      await fb.usersCollection.doc(user.uid).set({
+        username: form.username
+      })
+
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    },
+  },
   modules: {},
 });
