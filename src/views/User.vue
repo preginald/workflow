935cd59cc034d70f744dc1a2031ad91883e6717e
preginald <peter@reginald.com.au>
2021-01-08 06:18:08 +0000

    add icons and grouped list

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,14 +2,25 @@
   <v-container>
     <Heading />
   <v-card>
-    <v-list-item two-line v-for="(doc, i) in userDocs" :key="i">
-      <v-list-item-content>
-        <v-list-item-title><a @click.prevent="loadUserDoc(doc)" :href="docLink(doc)">{{
+    <v-list dense>
+      <v-subheader>Subheader</v-subheader>
+      <v-list-item-group v-model="selected">
+        <v-list-item two-line v-for="(doc, i) in userDocs" :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="status(doc.status)"></v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>
+              <a @click.prevent="loadUserDoc(doc)" :href="docLink(doc)">{{
                 doc.title
-              }}</a></v-list-item-title>
-        <v-list-item-subtitle>{{doc.description}}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+              }}</a>
+            </v-list-item-title>
+            <v-list-item-subtitle>{{doc.description}}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-card>
      <!-- <div @click="uetchUserDocs()">{{ docs }}</div> -->
   </v-container>
@@ -23,6 +34,7 @@ export default {
   computed: mapState(["userDocs"]),
   components: { Heading },
   data: () => ({
+    selected: '',
   }),
   mounted() {
     this.init()
@@ -31,6 +43,21 @@ export default {
     ...mapActions(['fetchUserDocs','loadUserDoc']),
     init(){
       this.fetchUserDocs(this.$route.params)
+    },
+    status(status) {
+      let icon = ""
+      switch(status){
+        case 'publish':
+          icon = "mdi-file-outline"
+        break;
+        case 'draft':
+          icon = "mdi-file-edit-outline"
+        break;
+        case 'delete':
+          icon = "mdi-file-remove-outline"
+        break;
+      }
+      return icon
     },
     docLink(doc) {
       return this.$route.params.userName + "/" + doc.slug;
