7ddcded1aa2881cb0c2d6f26c6b9fe8f170ef05b
preginald <peter@reginald.com.au>
2021-01-09 10:50:50 +0000

    move heading code to component

--- src/components/documents/Heading.vue
+++ src/components/documents/Heading.vue
@@ -1,26 +1,29 @@
 <template>
-  <v-row>
-    <v-col>
-      <h2 class="text-h6">
-        <router-link class="text-decoration-none" :to="userLink(activeDoc.username)">{{
-          activeDoc.username
-        }}</router-link>
-        /
-        {{ activeDoc.title }}
-      </h2>
-    </v-col>
-  </v-row>
+    <v-row v-if="Object.keys(activeDoc).length">
+      <v-col>
+        <h2 class="text-h6">
+          <router-link class="text-decoration-none" :to="userLink">{{
+            activeDoc.username
+            }}</router-link>
+          <span v-if="activeDoc.title"> / </span >
+          <router-link class="text-decoration-none" :to="activeDoc.slug">{{
+            activeDoc.title
+            }}</router-link>
+        </h2>
+      </v-col>
+    </v-row>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState } from "vuex";
 
 export default {
-  computed: mapState(["activeDoc"]),
+  computed: {
+    ...mapState(["activeDoc","userLink"]),
+  },
   components: {},
   data: () => ({}),
   methods: {
-    ...mapActions(['userLink']),
   },
 };
 </script>
