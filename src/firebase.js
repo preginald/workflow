620732e591daf740b3a881b19ef55c81666ecf23
preginald <peter@reginald.com.au>
2021-01-15 02:05:51 +0000

    move firebaseConfig to a module

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,7 @@
 import firebase from 'firebase'
 import 'firebase/auth'
 import 'firebase/firestore'
-
-// firebase init - add your own config here
-const firebaseConfig = {
-  apiKey: "AIzaSyCRikWNRskGXxWcvmk-Lj9mDV96TOLrSHg",
-  authDomain: "workflow-a.firebaseapp.com",
-  projectId: "workflow-a",
-   storageBucket: "workflow-a.appspot.com",
-   messagingSenderId: "612098345904",
-   appId: "1:612098345904:web:a7df21a1193600d1eee475",
-   measurementId: "G-HVBJCNKTDC"
- };
+import { firebaseConfig } from './firebaseConfig'
 
 firebase.initializeApp(firebaseConfig)
 
