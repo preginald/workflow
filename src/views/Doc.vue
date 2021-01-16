a18ea2ba0f0ab071f506471b21eeb8433a643294
preginald <peter@reginald.com.au>
2021-01-16 09:43:07 +0000

    move task interpreter to mixin

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -163,8 +163,10 @@
 <script>
 import { mapState, mapMutations, mapActions } from "vuex";
 import Heading from "../components/documents/Heading";
+import { taskInterpreter } from "../mixins/interpreter.js"
 
 export default {
+  mixins: [taskInterpreter],
   computed: {
     ...mapState(["activeDoc", "userLink","userProfile", "isOwner", "docValidation","taskTypes","taskInputHint"]),
   },
@@ -296,51 +298,6 @@ export default {
     },
     setTaskType(task,type){
       task.type = type
-    },
-    taskInterpreter(task) {
-      if(task){
-      const openTag = "<" + this.activeDoc.variableTag + ">"
-      const closeTag = "</" + this.activeDoc.variableTag + ">"
-
-      let n = task.search(openTag);
-      if (n == -1) {
-        return task;
-      } else {
-        let open = task.split(openTag)
-        let n = task.split(closeTag);
-        let newTask = "";
-        if(open.length == n.length){
-        n.forEach((e) => {
-          let x = e.search(openTag);
-          if (x == -1) {
-            newTask += e;
-          } else {
-            let variable = this.getVariable(e);
-            let value = this.getValue(variable);
-            newTask += e.replace(openTag+ variable, value);
-          }
-        });
-        return newTask;
-
-        }
-      }
-
-      } else {
-        return task
-      }
-    },
-    getVariable(task) {
-      const openTag = "<" + this.activeDoc.variableTag + ">"
-      let n = task.search(openTag);
-
-      if (n > -1) {
-        let variable = task.substring(n + openTag.length, task.length);
-        return variable;
-      }
-    },
-    getValue(name) {
-      let n = this.activeDoc.inputs.find((e) => e.name == name);
-      return n.value;
     },
   },
   watch: {
