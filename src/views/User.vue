0d996ad72c5acb7797de58ba438a7ba6ba58cda4
preginald <peter@reginald.com.au>
2021-01-17 06:15:02 +0000

    replace loader with skeleton loader component

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,41 +2,45 @@
   <v-container>
     <Heading />
     <v-divider class="my-3"></v-divider>
-  <v-card v-if="userDocs">
-    <v-list dense>
-      <v-subheader>{{ userDocs.length }} docs</v-subheader>
-      <v-list-item-group v-model="selected">
-        <v-list-item @click.prevent="loadUserDoc(doc)" two-line v-for="(doc, i) in userDocs" :key="i">
-          <v-list-item-icon>
-            <v-icon v-text="status(doc.status)"></v-icon>
-          </v-list-item-icon>
+    <v-row>
+      <v-col sm="12" md="9" lg="7">
+        <v-card v-if="userDocs">
+          <v-list dense>
+            <v-subheader>{{ userDocs.length }} docs</v-subheader>
+            <v-list-item-group v-model="selected">
+              <v-list-item @click.prevent="loadUserDoc(doc)" two-line v-for="(doc, i) in userDocs" :key="i">
+                <v-list-item-icon>
+                  <v-icon v-text="status(doc.status)"></v-icon>
+                </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>
-              <a  :href="docLink(doc)">{{
-                doc.title
-              }}</a>
-            </v-list-item-title>
-            <v-list-item-subtitle>{{doc.description}}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-card>
-  <v-card v-else>
-    <v-icon class="mdi-spin">mdi-loading</v-icon>
-  </v-card>
-     <!-- <div @click="uetchUserDocs()">{{ docs }}</div> -->
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <a  :href="docLink(doc)">{{
+                      doc.title
+                    }}</a>
+                  </v-list-item-title>
+                  <v-list-item-subtitle>{{doc.description}}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+        <v-card v-else>
+          <Loader />
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import { mapState,  mapActions } from "vuex"
 import Heading from "../../src/components/users/Heading";
+import Loader from "../components/documents/Loader";
 
 export default {
   computed: mapState(["userDocs"]),
-  components: { Heading },
+  components: { Heading, Loader },
   data: () => ({
     selected: '',
   }),
