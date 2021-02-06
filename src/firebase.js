7cea302546ddda9aaacc666324a8179bda1eb144
preginald <peter@reginald.com.au>
2021-02-06 17:10:38 +1100

    add commands collection

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,18 @@
-import firebase from 'firebase'
-import 'firebase/auth'
-import 'firebase/firestore'
-import { firebaseConfig } from './firebaseConfig'
+import firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // utils
-const db = firebase.firestore()
-const auth = firebase.auth()
+const db = firebase.firestore();
+const auth = firebase.auth();
 
 // collection references
-const usersCollection = db.collection('users')
-const docsCollection = db.collection('docs')
+const usersCollection = db.collection("users");
+const docsCollection = db.collection("docs");
+const commandsCollection = db.collection("commands");
 
 // export utils/refs
-export {
-  db,
-  auth,
-  usersCollection,
-  docsCollection,
-}
-
+export { db, auth, usersCollection, docsCollection, commandsCollection };
